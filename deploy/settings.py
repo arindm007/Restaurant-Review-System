72b8cd0f0a41d7cf4718b7daf84601ac3e9a8265
Arindam Chakraborty <arindamchakraborty6.10@gmail.com>
2022-06-17 21:21:06 +0530

    Allowed host

--- deploy/settings.py
+++ deploy/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-$1_i$-z@!4o#sd$aa(f@a+u&eh&dke9e!u9fjl17g&&2w&(e8s'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: don't run with debug turned on in prodgituction!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['restaurant-review-system-a007.herokuapp.com']
 
 
 # Application definition
