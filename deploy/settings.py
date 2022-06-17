f663641b951fbf8841cbe499de053e82f548f647
Arindam Chakraborty <arindamchakraborty6.10@gmail.com>
2022-06-17 22:48:52 +0530

    collect static added

--- deploy/settings.py
+++ deploy/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -123,5 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
