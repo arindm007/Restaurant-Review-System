49b11a4067a6bcde50919879318a20ca81352827
Gaurav Mukherjee <gaurav2001.rig@gmail.com>
2022-06-19 14:05:05 +0530

    Removing extra code

--- deploy/settings.py
+++ deploy/settings.py
@@ -131,31 +131,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 django_heroku.settings(locals())
-
-'''def patch_broken_pipe_error():
-    """Monkey Patch BaseServer.handle_error to not write
-    a stacktrace to stderr on broken pipe.
-    http://stackoverflow.com/a/22618740/362702"""
-    import sys
-    from SocketServer import BaseServer
-    from wsgiref import handlers
-
-    handle_error = BaseServer.handle_error
-    log_exception = handlers.BaseHandler.log_exception
-
-    def is_broken_pipe_error():
-        type, err, tb = sys.exc_info()
-        return repr(err) == "error(32, 'Broken pipe')"
-
-    def my_handle_error(self, request, client_address):
-        if not is_broken_pipe_error():
-            handle_error(self, request, client_address)
-
-    def my_log_exception(self, exc_info):
-        if not is_broken_pipe_error():
-            log_exception(self, exc_info)
-
-    BaseServer.handle_error = my_handle_error
-    handlers.BaseHandler.log_exception = my_log_exception
-
-patch_broken_pipe_error()'''
